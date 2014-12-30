1c4b6c43ad84950a09f4da5f7f5b429e7147cd17
AJ ONeal <awesome@coolaj86.com>
2014-12-29 23:10:29 -0700

    move server implementation out

--- server.js
+++ server.js
@@ -2,139 +2,141 @@
 
 var connect = require('connect')
   , urlrouter = require('urlrouter')
-  , app = connect()
   , signin = require('./signin').signin
   , DB = require('./db')
-  , config = require('./config')
-  , db
   , PromiseA = require('bluebird').Promise
   ;
 
-function saveAndQuit() {
-  db.save().then(function () {
-    process.exit();
-  }).catch(function (e) {
-    console.error(e);
-    process.exit();
-  });
-}
+//process.on('uncaughtException', saveAndQuit);
+module.exports.create = function (config /*, server*/) {
+  var app = connect()
+    , db
+    ;
 
-function login(auth, preset) {
-  return new PromiseA(function (resolve, reject) {
-    var errmsg
-      ;
-
-    // username must be at least 4, password at least 8
-    ['username', 'password'].every(function (k) {
-      if ('string' !== typeof auth[k]) {
-        errmsg = { message: k + " must be specified" };
-
-        return false;
-      }
-
-      if (auth[k].length < 4) {
-        errmsg = { message: k + " is too short" };
-
-        return false;
-      }
-
-      return true;
+  function saveAndQuit() {
+    db.save().then(function () {
+      process.exit();
+    }).catch(function (e) {
+      console.error(e);
+      process.exit();
     });
-
-    if (errmsg) {
-      reject(errmsg);
-      return;
-    }
-
-    resolve();
-  }).then(function () {
-    return signin(auth.username, auth.password).then(function (jar) {
-      if (preset) {
-        return { jar: jar };
-      }
-
-      return db.set(auth.username, auth.password).then(function (token) {
-        return { token: token, jar: jar };
-      });
-    });
-  });
-}
-
-function route(rest) {
-  function requireInit(req, res, next) {
-    if (!db) {
-      res.error({ message: "init not complete" });
-      return;
-    }
-    
-    next();
   }
 
-  rest.post('/api/init', function (req, res) {
-    var body = req.body
-      ;
+  function login(auth, preset) {
+    return new PromiseA(function (resolve, reject) {
+      var errmsg
+        ;
 
-    if (db) {
-      res.error({ message: "init already complete" });
-      return;
-    }
+      // username must be at least 4, password at least 8
+      ['username', 'password'].every(function (k) {
+        if ('string' !== typeof auth[k]) {
+          errmsg = { message: k + " must be specified" };
 
-    if (!DB.test(body.secret, config.shadow)) {
-      res.error({ message: "bad secret" });
-      return;
-    }
+          return false;
+        }
 
-    db = DB.create({ secret: body.secret, salt: config.salt });
-    res.send({ success: true });
-  });
+        if (auth[k].length < 4) {
+          errmsg = { message: k + " is too short" };
 
-  rest.post('/api/login', requireInit, function (req, res) {
-    var body = req.body
-      ;
+          return false;
+        }
 
-    login(body).then(function (data) {
-      res.send({ token: data.token, jar: data.jar });
-    }, function (errmsg) {
-      res.error({ message: errmsg && errmsg.message || errmsg });
+        return true;
+      });
+
+      if (errmsg) {
+        reject(errmsg);
+        return;
+      }
+
+      resolve();
+    }).then(function () {
+      return signin(auth.username, auth.password).then(function (jar) {
+        if (preset) {
+          return { jar: jar };
+        }
+
+        return db.set(auth.username, auth.password).then(function (token) {
+          return { token: token, jar: jar };
+        });
+      });
     });
-  });
+  }
 
-  rest.post('/api/passthru', requireInit, function (req, res) {
-    var body = req.body
-      ;
-
-    if ('string' !== typeof body.token) {
-      res.error({ message: "no token supplied" });
+  function route(rest) {
+    function requireInit(req, res, next) {
+      if (!db) {
+        res.error({ message: "init not complete" });
+        return;
+      }
+      
+      next();
     }
 
-    db.get(body.token).then(function (data) {
-      return login(data, true);
-    }).then(function (data) {
-      res.send({ jar: data.jar });
-    }).catch(function (errmsg) {
-      res.error({ message: errmsg });
+    rest.post('/api/init', function (req, res) {
+      var body = req.body
+        ;
+
+      if (db) {
+        res.error({ message: "init already complete" });
+        return;
+      }
+
+      if (!DB.test(body.secret, config.shadow)) {
+        res.error({ message: "bad secret" });
+        return;
+      }
+
+      db = DB.create({ secret: body.secret, salt: config.salt });
+      res.send({ success: true });
     });
-  });
 
-  rest.post('/api/restart', requireInit, function (req, res) {
-    res.send({ success: true });
-    saveAndQuit();
-  });
-}
+    rest.post('/api/login', requireInit, function (req, res) {
+      var body = req.body
+        ;
 
-app.use(require('body-parser').json({
-  strict: true
-, inflate: true
-, limit: 100 * 1024
-, reviver: undefined
-, type: 'json'
-, verify: undefined
-}));
-app.use(require('connect-send-json').json());
-app.use(require('connect-send-error').error());
-app.use(urlrouter(route));
+      login(body).then(function (data) {
+        res.send({ token: data.token, jar: data.jar });
+      }, function (errmsg) {
+        res.error({ message: errmsg && errmsg.message || errmsg });
+      });
+    });
 
-//process.on('uncaughtException', saveAndQuit);
-module.exports.create = function (/*server*/) {
+    rest.post('/api/passthru', requireInit, function (req, res) {
+      var body = req.body
+        ;
+
+      if ('string' !== typeof body.token) {
+        res.error({ message: "no token supplied" });
+      }
+
+      db.get(body.token).then(function (data) {
+        return login(data, true);
+      }).then(function (data) {
+        res.send({ jar: data.jar });
+      }).catch(function (errmsg) {
+        res.error({ message: errmsg });
+      });
+    });
+
+    rest.post('/api/restart', requireInit, function (req, res) {
+      res.send({ success: true });
+      saveAndQuit();
+    });
+  }
+
+  app.use(require('body-parser').json({
+    strict: true
+  , inflate: true
+  , limit: 100 * 1024
+  , reviver: undefined
+  , type: 'json'
+  , verify: undefined
+  }));
+  app.use(require('connect-send-json').json());
+  app.use(require('connect-send-error').error());
+
+  app.use(urlrouter(route));
+
   return PromiseA.resolve(app);
 };
